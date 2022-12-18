5420d4c92d7c43d4a48ee1a4d209dbf049e58c41
Ramakant Jangir <jangirravin0@gmail.com>
2022-12-19 00:05:52 +0530

    updt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Education from "./pages/Education";
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/personal-portfolio">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
