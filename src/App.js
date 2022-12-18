5305c627dbf871c1402146340c3a36724932dd88
Ramakant Jangir <jangirravin0@gmail.com>
2022-12-18 23:48:19 +0530

    another cmt

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/personal-portfolio" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/education" element={<Education />} />
